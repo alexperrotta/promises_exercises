2bc8e943ae58cd99c55d05b0775ac5d2aee469ff
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-04-29 14:39:09 -0400

    changes to part 4

--- script.js
+++ script.js
@@ -61,10 +61,51 @@ addNumbers(2, 2);
 
 
 
+// Part 4 - Promisify
+
+// The function below uses callbacks to report the result of dropping buttered toast on the floor.
+
+let dropButteredToastOnFloor = (success, failure) => {
+   let drop = Math.random();
+   if (drop < 0.5) {
+      console.log("Landed butter-side up!");
+      success();
+   } else {
+      console.log("Landed butter-side down...");
+      failure();
+   }
+}
+
+
+// To react to the dropping of toast, we have to provide callbacks for each possibility: 
+// dropping butter side up, or butter side down.
+
+dropButteredToastOnFloor(()=>{
+   alert('Whew, that was close!');
+}, ()=>{
+   alert('Well shucks, there goes my toast...');
+});
 
 
 
+// YOUR TASK- Write a function called dropToastPromisified(), make it return a new Promise(), 
+// and use the dropButteredToastOnFloor() function to call either resolve() or reject(). 
+// If done correctly, you should be able to run:
 
+dropToastPromisified()
+   .then(()=>{
+      alert('Whew, that was close!');
+   }).catch(()=>{
+      alert('Well shucks, there goes my toast...');
+   });
+
+
+function dropToastPromisified(){
+	return new Promise(function(resolve, reject){
+		
+	});
+
+};
 
 
 
